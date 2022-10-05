3fed41e93042acd73d862e1b2e66c81c6cc2462b
NgoDinhHoangHa <hoanghadn0123@gmail.com>
2022-10-05 14:08:27 +0700

    them bmi

--- app/src/main/java/com/example/myapplication/ViewModel/WalkthroughViewMOdel.java
+++ app/src/main/java/com/example/myapplication/ViewModel/WalkthroughViewMOdel.java
@@ -1,13 +1,13 @@
 package com.example.myapplication.ViewModel;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.BaseObservable;
+
 
 import android.os.Bundle;
 
 import com.example.myapplication.R;
 
-public class WalkthroughViewMOdel extends BaseObservable {
+public class WalkthroughViewMOdel  {
 
 
 }
